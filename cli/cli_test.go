01409fba7a1554cc01b251c63a5c0515e829e838
zeeraw <zee@vall.in>
2019-01-06 13:10:35 +0100

    test: command line tool has some test coverage

---
+++ cli/cli_test.go
@@ -0,0 +1,139 @@
+package cli_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/zeeraw/riksbank/cli"
+	"github.com/zeeraw/riksbank/swea"
+)
+
+const (
+	bin = "riksbank"
+)
+
+var (
+	tool cli.Tool
+)
+
+func TestMain(m *testing.M) {
+	tool = cli.Tool{
+		API: swea.NewMock(),
+	}
+	os.Exit(m.Run())
+}
+
+func Test_Run(t *testing.T) {
+	cases := []struct {
+		name   string
+		args   []string
+		errors bool
+	}{
+		{
+			name:   "(none)",
+			args:   []string{},
+			errors: true,
+		},
+		{
+			name: "(only bin)",
+			args: []string{
+				bin,
+			},
+			errors: false,
+		},
+		{
+			name: "rates",
+			args: []string{
+				bin,
+				"rates",
+			},
+			errors: true,
+		},
+		{
+			name: "rates with one series",
+			args: []string{
+				bin,
+				"rates",
+				"-s",
+				"SEKNOKPMI",
+			},
+			errors: false,
+		},
+		{
+			name: "series",
+			args: []string{
+				bin,
+				"series",
+			},
+			errors: false,
+		},
+		{
+			name: "groups",
+			args: []string{
+				bin,
+				"groups",
+			},
+			errors: false,
+		},
+		{
+			name: "days",
+			args: []string{
+				bin,
+				"days",
+			},
+			errors: false,
+		},
+		{
+			name: "exchange",
+			args: []string{
+				bin,
+				"exchange",
+			},
+			errors: false,
+		},
+		{
+			name: "exchange rates",
+			args: []string{
+				bin,
+				"exchange",
+				"rates",
+			},
+			errors: true,
+		},
+		{
+			name: "exchange rates with currency pair",
+			args: []string{
+				bin,
+				"exchange",
+				"rates",
+				"-c",
+				"SEK/NOK",
+			},
+			errors: false,
+		},
+		{
+			name: "exchange series",
+			args: []string{
+				bin,
+				"exchange",
+				"series",
+			},
+			errors: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			err := tool.Run(c.args)
+			if c.errors {
+				if err == nil {
+					t.Errorf("command should error but didn't")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("should not have error: %v", err)
+				}
+			}
+		})
+	}
+}
